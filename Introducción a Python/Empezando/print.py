aebbcb729641effbca5f255620604d2b35efe445
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-02-04 16:57:02 -0600

    Add files via upload

---
+++ Introducción a Python/Empezando/print.py
@@ -0,0 +1,25 @@
+# Python tiene una sintaxis simple, lo que significa que es 
+# fácil de escribir, leer y aprender.
+
+
+print("Welcome to Python Programming\n")
+
+# En este ejemplo, la función print() muestra el mensaje "Welcome to Python Programming" en la pantalla.    
+
+# La instrucción print() se puede reutilizar para mostrar varios mensajes de texto.
+# Imagina utilzar este simple pedazo de código para decir en un videjuego que haz subido de nivel.
+
+print("Level Up!\n")
+
+# Ahora imagina que creas una aplicación de correos electrónicos.
+
+print("New Email\n")
+
+# Los humanos usan código para dar instrucciones a las computadoras.
+
+# IMPORTANTE : Nunca olvides escribir correctamente los paréntesis de la instrucción print().
+
+# IMPORTANTE : Nunca olvides escribir correctamente las comillas de la instrucción print().
+
+# Si olvidas las comillas o los paréntesis, la computadora no podra ejecutar tu código.
+
