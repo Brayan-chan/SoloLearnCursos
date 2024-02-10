4161c092295ae43d2c4babb44320579482384e00
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-02-10 11:37:12 -0600

    Add files via upload

---
+++ Memoria y variables/item.py
@@ -0,0 +1,18 @@
+#Para crear una variable, solo se necesita darle un nombre.
+
+item = "bike"
+print(item)
+item = "phone"
+print(item)
+
+#Imagina que quieres almacenar el nombre de un lugar en una variable.
+
+country = "México"
+print(country)
+
+#PUEDES PENSAR QUE UNA VARIABLE ES UNA CAJA QUE CONTIENE ALGUNA INFORMACIÓN.
+#Nombre : conuntry
+#Valor : "México"
+
+
+
