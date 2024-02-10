5f49a0b224464552717a5b64869c632bc2caf8a4
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-02-10 11:51:04 -0600

    Add files via upload

---
+++ practica2.py
@@ -0,0 +1,13 @@
+#Imagina almacenar el saldo de un cliente en una variable.
+
+saldoInvertido = 2000
+print("Su saldo invertido es:",saldoInvertido)
+
+#Imagina almacenar los datos de un usuario utilizando variables.
+
+nombre = "John"
+edad = 30
+correo = "p8J2C@example.com"
+
+print("============================================")
+print("Los datos del usuario son:","\nnombre:",nombre,"\nedad:",edad,"\nemail:",correo,"\n============================================")
