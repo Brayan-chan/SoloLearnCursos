f97b2bb946986cdd580af5415aec5b99dd489849
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-02-10 22:56:10 -0600

    Add files via upload

---
+++ Datos de texto/Cadenas.py
@@ -0,0 +1,28 @@
+#Para representar una cadena de texto solo se rodea de comillas una cadena de texto.abs
+
+"Cadena de texto"
+
+#Las cadenas se pueden almacenar en variables.
+
+Company = "Apple"
+print(Company)
+
+dato_importante = '\nEn Python, tanto las comillas simples como las dobles\npueden usarse para definir cadenas...'
+print(dato_importante)
+
+#Puedes agregar tantas variables como necesites para darle instrucciones a la computadora.
+
+movie = "Toy Story"
+author = "John Lasseter"
+print("\nMy favorite movie is:",movie)
+print("The author of the movie is:",author)
+
+#Sin darte cuenta estuviste trabajando con declaraciones
+
+#Dos declaraciones podrian ser:
+
+nombre = "John"
+edad = 30
+
+print("\nMy name is:",nombre)
+print("My age is:",edad)
